f0c2ad137b2d6de419138b152cf7517ceceb2258
Siddharth <siddharth.mtc09@gmail.com>
2025-08-05 19:31:41 +0530

    feat: implement ApiError and asyncHandler utility classes

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,23 @@
+const asyncHandler = (requestHandler)=>{
+    (req,res,next)=>{
+        Promise.resolve(requestHandler(req,res,next))
+        .catch((error) => next (error))
+    }
+}
+
+
+return {asyncHandler}
+
+
+
+
+// const asyncHandler = (fn) => async (req,res,error) => {
+//     try {
+//         await fn(req, res, error); 
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success:false,
+//             message: error.message
+//         })
+//     }
+// }
